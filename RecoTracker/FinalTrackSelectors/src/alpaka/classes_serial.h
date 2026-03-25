9b28d89ce12ac20d98a1cf3eadfb4bc4a40158b5
Jade Chismar <jchismar@ucsd.edu>
2026-03-25 05:56:57 -0700

    clean up warnings

--- RecoTracker/FinalTrackSelectors/src/alpaka/classes_serial.h
+++
@@ -1,9 +0,0 @@
-#ifndef RecoTracker_FinalTrackSelectors_src_alpaka_classes_serial_h
-#define RecoTracker_FinalTrackSelectors_src_alpaka_classes_serial_h
-
-#include "DataFormats/Common/interface/DeviceProduct.h"
-#include "DataFormats/Common/interface/Wrapper.h"
-#include "RecoTracker/FinalTrackSelectors/interface/alpaka/TrackFeaturesDeviceCollection.h"
-#include "RecoTracker/FinalTrackSelectors/interface/alpaka/TrackScoresDeviceCollection.h"
-
-#endif
